047d88b88e65f4af9d408df90069e23663190fc5
Navneet Hegde <navneet.hegde@hotmail.com>
2024-08-25 12:15:46 -0400

    Updated Readme file

--- OpenCode.Tests/StringExtension/StringExtension.Decimal.UnitTest.cs
+++ OpenCode.Tests/StringExtension/StringExtension.Decimal.UnitTest.cs
@@ -6,34 +6,34 @@ namespace OpenCode.Tests
     {
         #region ParseDecimal
         [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("abcd!@#$")]
-        public void ParseToDecimal_ShouldReturnDefault0(string input)
+        [InlineData(null, 0)]
+        [InlineData("", 0)]
+        [InlineData("abcd!@#$", 0)]
+        public void ParseToDecimal_ShouldReturnDefault0(string input, decimal expected)
         {
             var result = input.ParseToDecimal();
 
-            Assert.Equal(0, result);
+            Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("abcd!@#$1234")]
-        public void ParseToDecimal_ShouldReturnDefault5(string input)
+        [InlineData(null, 5)]
+        [InlineData("", 5)]
+        [InlineData("abcd!@#$1234", 5)]
+        public void ParseToDecimal_ShouldReturnDefault5(string input, decimal expected)
         {
-            var result = input.ParseToDecimal(5);
+            var result = input.ParseToDecimal(expected);
 
-            Assert.Equal(5, result);
+            Assert.Equal(expected, result);
         }
 
         [Theory]
-        [InlineData("1")]
-        [InlineData("1.0")]
-        [InlineData("01.0")]
-        public void ParseToDecimal_ShouldParseInput(string input)
+        [InlineData("1", 5)]
+        [InlineData("1.0", 5)]
+        [InlineData("01.0", 5)]
+        public void ParseToDecimal_ShouldParseInput(string input, decimal expected)
         {
-            var result = input.ParseToDecimal(5);
+            var result = input.ParseToDecimal(expected);
 
             Assert.Equal(decimal.Parse(input), result);
         }
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # OpenCode
 
-Repository of utility code snippets.
+The Custom C# Extension Methods Library is a collection of thoughtfully designed extension methods aimed at enhancing and simplifying common operations on strings and other .NET types. This project provides developers with a set of reusable, intuitive, and efficient methods that can be easily integrated into any C# project. The goal is to extend the functionality of standard .NET classes by adding value through custom methods that are frequently needed but not available in the base class libraries.
+
+
+## Download/Install
+
+[OpenCode Nuget](https://www.nuget.org/packages/OpenCode/)
+
+## Usage
+
+### Example 1: `ParseToBool`
+```csharp
+string input = "Hello";
+string padded = input.ParseToBool(); // Output: false
